8fb4bb094e3de9c10347acac50e6620cadcc258b
Anand Doshi <hello@apd.is>
2017-10-24 15:53:17 -0400

    code cleanup: split code into functions and consolidated function calls into run()

--- madlib_generator.py
+++ madlib_generator.py
@@ -1,25 +1,54 @@
 # code developed by Jackie Cohen; revised by Paul Resnick
-import nltk # requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
+# cleanup by Anand Doshi
+
+import nltk
+# nltk requires some downloading/installing dependencies to use all its features;
+# numpy is especially tricky to install
+
 import random
 
 debug = False #True
-# Here is a comment for SI206
-# get file from user to make mad lib out of
-if debug:
-	print "Getting information from file madlib_test.txt...\n"
-fname = "madlib_test.txt" # need a file with this name in directory
 
-f = open(fname, 'r')
-para = f.read()
-tokens = nltk.word_tokenize(para)
-tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
-if debug:
-	print "First few tagged tokens are:"
-	for tup in tagged_tokens[:5]:
-		print tup
+def read_file(fname=None):
+    # Here is a comment for SI206
+    # get file from user to make mad lib out of
+    if debug:
+    	print "Getting information from file madlib_test.txt...\n"
 
-tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
-substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
+    if not fname:
+        fname = "madlib_test.txt" # need a file with this name in directory
+
+    with open(fname, 'r') as f:
+        para = f.read()
+
+    return para
+
+def tokenize(para):
+    tokens = nltk.word_tokenize(para)
+    tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+    if debug:
+    	print "First few tagged tokens are:"
+    	for tup in tagged_tokens[:5]:
+    		print tup
+
+    return tagged_tokens
+
+def ask_and_substitute(tagged_tokens):
+    tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
+    substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
+
+
+    final_words = []
+
+    for (word, tag) in tagged_tokens:
+    	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+    		final_words.append(spaced(word))
+    	else:
+    		new_word = raw_input("Please enter %s:\n" % (tagmap[tag]))
+    		final_words.append(spaced(new_word))
+
+    return final_words
+
 
 def spaced(word):
 	if word in [",", ".", "?", "!", ":"]:
@@ -27,14 +56,11 @@ def spaced(word):
 	else:
 		return " " + word
 
-final_words = []
+def run():
+    para = read_file()
+    tagged_tokens = tokenize(para)
+    final_words = ask_and_substitute(tagged_tokens)
+    print "".join(final_words)
 
-
-for (word, tag) in tagged_tokens:
-	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
-		final_words.append(spaced(word))
-	else:
-		new_word = raw_input("Please enter %s:\n" % (tagmap[tag]))
-		final_words.append(spaced(new_word))
-
-print "".join(final_words)
+if __name__ == '__main__':
+    run()
